914d33cf74993c8a941a43dc451c8518be6e7636
Cauan Cabral <cauan@radig.com.br>
2010-04-05 14:45:57 -0400

    Adicionado inicio do lab3

---
+++ lab3/barrel_shifter.vhd
@@ -0,0 +1,9 @@
+entity barrel_shifter is
+	port(
+		din1, din2, din3, din4: in bit;
+		desloc1, desloc2: in bit;
+		dout1, dout2, dout3, dout4: out bit;
+	);
+end entity;
+
+
---
+++ lab3/mux2b.vhd
@@ -0,0 +1,9 @@
+entity mux2b is
+	port (a, b, sel: in bit; o: out bit);
+end entity;
+
+architecture comportamental of mux2b is
+begin
+	o <= a when sel = '0' else
+		b when sel = '1';
+end;
